a9992c6f1e5ed41919e1491572cfca6aa0eeebd8
angelvtbg <ngellllv@gmail.com>
2024-11-27 08:21:06 +0700

    song

--- index.php
+++ index.php
@@ -18,6 +18,7 @@
 
         <!--=============== CSS ===============--> 
         <link rel="stylesheet" href="assets/css/styles.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
 
         <title>Muzzy</title>
     </head>
@@ -54,14 +55,109 @@
         toggleSearch('search-bar', 'search-button')
     </script>
 
+    <script>
+        const
+            // image = document.getElementById('cover'),
+            // title = document.getElementById('music-title'),
+            // artist = document.getElementById('music-artist'),
+            currentTimeEl = document.getElementById('current-time'),
+            durationEl = document.getElementById('duration'),
+            progress = document.getElementById('progress'),
+            playerProgress = document.getElementById('player-progress'),
+            prevBtn = document.getElementById('prev'),
+            nextBtn = document.getElementById('next'),
+            playBtn = document.getElementById('play'),
+            background = document.getElementById('bg-img');
+
+        const music = new Audio();
+
+        const songs = [
+            {
+                path: 'assets/song/1.mp3',
+                // displayName: 'The Charmer\'s Call',
+                // cover: 'assets/img/mltr.jpg',
+                // artist: 'Hanu Dixit',
+            }
+        ];
+
+        let musicIndex = 0;
+        let isPlaying = false;
+
+        function togglePlay() {
+            if (isPlaying) {
+                pauseMusic();
+            } else {
+                playMusic();
+            }
+        }
+
+        function playMusic() {
+            isPlaying = true;
+            // Change play button icon
+            playBtn.classList.replace('fa-play', 'fa-pause');
+            // Set button hover title
+            playBtn.setAttribute('title', 'Pause');
+            music.play();
+        }
+
+        function pauseMusic() {
+            isPlaying = false;
+            // Change pause button icon
+            playBtn.classList.replace('fa-pause', 'fa-play');
+            // Set button hover title
+            playBtn.setAttribute('title', 'Play');
+            music.pause();
+        }
+
+        function loadMusic(song) {
+            music.src = song.path;
+            // title.textContent = song.displayName;
+            // artist.textContent = song.artist;
+            // image.src = song.cover;
+            // background.src = song.cover;
+        }
+
+        function changeMusic(direction) {
+            musicIndex = (musicIndex + direction + songs.length) % songs.length;
+            loadMusic(songs[musicIndex]);
+            playMusic();
+        }
+
+        function updateProgressBar() {
+            const { duration, currentTime } = music;
+            const progressPercent = (currentTime / duration) * 100;
+            progress.style.width = `${progressPercent}%`;
+
+            const formatTime = (time) => String(Math.floor(time)).padStart(2, '0');
+            durationEl.textContent = `${formatTime(duration / 60)}:${formatTime(duration % 60)}`;
+            currentTimeEl.textContent = `${formatTime(currentTime / 60)}:${formatTime(currentTime % 60)}`;
+        }
+
+        function setProgressBar(e) {
+            const width = playerProgress.clientWidth;
+            const clickX = e.offsetX;
+            music.currentTime = (clickX / width) * music.duration;
+        }
+
+        playBtn.addEventListener('click', togglePlay);
+        prevBtn.addEventListener('click', () => changeMusic(-1));
+        nextBtn.addEventListener('click', () => changeMusic(1));
+        music.addEventListener('ended', () => changeMusic(1));
+        music.addEventListener('timeupdate', updateProgressBar);
+        playerProgress.addEventListener('click', setProgressBar);
+
+        loadMusic(songs[musicIndex]);
+    </script>
+
     <!--=============== SCROLL REVEAL ===============-->
     <script src="assets/js/scrollreveal.min.js"></script>
 
     <!--=============== SWIPER JS ===============-->
     <script src="assets/js/swiper-bundle.min.js"></script>
 
-    <!--=============== MAIN JS ===============-->
+    <!--=============== JS ===============-->
     <script src="assets/js/main.js"></script>
+    <script src="assets/js/music.js"></script>
 
     </body>
 </html>
---
+++ partials/song.php
@@ -0,0 +1,86 @@
+<section class="home container" id="home">
+<!-- <div class="home__container bd-container bd-grid"> -->
+</section>
+
+<!--==================== SONG ====================-->
+<section class="section discount">
+<div class="discount__container container bd-grid">
+    <div>
+    <div class="music__container">
+        <div class="player-img">
+            <img src="./assets/img/mltr.jpg" class="active" id="cover">
+        </div>
+
+        <h2 id="" class="music__h2">The Actor</h2>
+        <h3 id="" class="music__h3">Michael Learns to Rock</h3>
+
+        <div class="player-progress" id="player-progress">
+            <div class="progress" id="progress"></div>
+            <div class="music-duration">
+                <span id="current-time">0:00</span>
+                <span id="duration">0:00</span>
+            </div>
+        </div>
+
+        <div class="player-controls">
+            <!-- <i class="fa-solid fa-backward" title="Previous" id="prev"></i> -->
+            <i class="fa-solid fa-play play-button" title="Play" id="play"></i>
+            <!-- <i class="fa-solid fa-forward" title="Next" id="next"></i> -->
+        </div>
+
+    </div>
+
+    <div class="song__data">
+        <h2 class="song__lyrics">
+            Album : Michael Learns to Rock <br>
+            Release Date : 1991-11-11 <br>
+            Producers : Oli Poulsen & Jens Hofman <br>
+            Label : Impact Medley Records <br>
+            Writer : Jascha Richter <br>
+        </h2>
+    </div>
+    </div>
+    
+    <h2 class="song__lyrics">
+        He takes you out and he takes you up <br>
+        'Cause he can show you so much <br>
+        I go to bed and tomorrow again <br>
+        There's a lot of work to be done <br>
+        He gives you gold and he will promise you <br>
+        The whole world will be yours <br>
+        I just can't tell you I love you so <br>
+        Even though my odds are low <br>
+        I'm not an actor, I'm not a star <br>
+        And I don't even have my own car <br>
+        But I'm hoping so much you'll stay <br>
+        That you will love me anyway <br>
+        The dirty games in the name of jokes <br>
+        This is the world he knows <br>
+        Watching the stars satisfies my soul <br>
+        Thinking of him makes me feel so cold <br>
+        The fancy cars and the restaurants <br>
+        You're just so fond of the man <br>
+        Sometimes I wonder if you are blind <br>
+        Can't you see he's got dirt on his mind <br>
+        I'm not an actor, I'm not a star <br>
+        And I don't even have my own car <br>
+        But I'm hoping so much you'll stay <br>
+        That you will love me anyway (anyway) <br>
+        I'm not an actor, I'm not a star <br>
+        And I don't even have my own car <br>
+        But I'm hoping so much you'll stay <br>
+        That you will love me anyway <br>
+        I'm not an actor, I'm not a star <br>
+        And I don't even have my own car <br>
+        But I'm hoping so much you'll stay <br>
+        That you will love me anyway (anyway) <br>
+        I'm not an actor, I'm not a star <br>
+        And I don't even have my own car <br>
+        But I'm hoping so much you'll stay <br>
+        That you will love me anyway <br>
+        I'm not an actor, I'm not a star <br>
+        And I don't even have my own car <br>
+        But I'm hoping so much you'll stay <br>
+    </h2>
+</div>
+</section>
